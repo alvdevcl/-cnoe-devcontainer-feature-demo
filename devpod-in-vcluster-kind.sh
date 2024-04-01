6ec300b5886a07ad0bcad200ba175495ad909e8a
aatchison <arron.atchison@gmail.com>
2024-04-01 16:13:46 -0700

    fix typos, add desc, use cache image

--- devpod-in-vcluster-kind.sh
+++ devpod-in-vcluster-kind.sh
@@ -13,11 +13,11 @@ devpod provider add kubernetes
 devpod up https://github.com/aatchison/cnoe-devcontainer-feature-demo --ide none --provider kubernetes
 
 # run examples
-ssh cnoe-devcontainer-feature-demo -C "./idpbuilder-example.sh"
+ssh cnoe-devcontainer-feature-demo.devpod -C "./idpbuilder-example.sh"
 ssh cnoe-devcontainer-feature-demo.devpod -C "./cnoe-cli-example.sh"
 
 sleep 10
 
-devpod delete idpbuilder
+devpod delete cnoe-devcontainer-feature-demo
 
 kind delete clusters kind

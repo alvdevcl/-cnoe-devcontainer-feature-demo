9543033de8cd136a97049c51b14737e6c787f7ab
aatchison <arron.atchison@gmail.com>
2024-04-01 15:56:43 -0700

    run kubectl the hard way. It looks like nanolayers don't work will with devpod

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,3 +1,10 @@
+# For Kubectl AMD64 / x86_64
+[ $(uname -m) = x86_64 ] && curl -sLO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+# For Kubectl ARM64
+[ $(uname -m) = aarch64 ] && curl -sLO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/arm64/kubectl"
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+
 # For devpod AMD64 / x86_64
 [ $(uname -m) = x86_64 ] && curl -sL https://github.com/loft-sh/devpod/releases/download/v0.5.4/devpod-linux-amd64 -o devpod
 # For devpod ARM64
